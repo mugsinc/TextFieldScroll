1f9cc1c3120e47d37d00c4cb9ae211c8026151a3
Mugunthan Balakrishnan <mugs@230i.com>
2015-11-30 13:13:16 +0530

    first commit

--- TextFieldScroll/ViewController.swift
+++ TextFieldScroll/ViewController.swift
@@ -8,16 +8,95 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var ScrollView: UIScrollView!
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textField1.delegate = self
+        textField2.delegate = self
+        textField3.delegate = self
+        textField4.delegate = self
+        
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        view.addGestureRecognizer(tap)
+        ScrollView.scrollEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerForKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func registerForKeyboardNotifications() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self,
+            selector: "keyboardWillBeShown:",
+            name: UIKeyboardWillShowNotification,
+            object: nil)
+        notificationCenter.addObserver(self,
+            selector: "keyboardWillBeHidden:",
+            name: UIKeyboardWillHideNotification,
+            object: nil)
+    }
+    
+    func keyboardWillBeShown(sender: NSNotification) {
+        let info: NSDictionary = sender.userInfo!
+        let value: NSValue = info.valueForKey(UIKeyboardFrameBeginUserInfoKey) as! NSValue
+        let keyboardSize: CGSize = value.CGRectValue().size
+        let contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height + 8, 0.0)
+        ScrollView.contentInset = contentInsets
+        ScrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    // Called when the UIKeyboardWillHideNotification is sent
+    func keyboardWillBeHidden(sender: NSNotification) {
+        let insets: UIEdgeInsets = UIEdgeInsetsMake(self.ScrollView.contentInset.top, 0, 0, 0)
+        
+        //let insets: UIEdgeInsets = UIEdgeInsetsZero
+        ScrollView.contentInset = insets
+        ScrollView.scrollIndicatorInsets = insets
+        
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        ScrollView.scrollEnabled = true
+        let scrollSize = CGSizeMake(view.frame.width, view.frame.height)
+        ScrollView.contentSize = scrollSize    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        ScrollView.scrollEnabled = false
     }
 
 
